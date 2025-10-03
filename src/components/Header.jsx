0e31f4d0dcac88bbe6e6d8fda68d8aa7d2b2b42c
Prashant <shishupal78008@gmail.com>
2025-10-03 17:59:59 +0530

    ff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     <div className='h-[7vh] flex items-center bg-[#1C1F2E] justify-between p-4'>
       <div className="logo h-[7vh] w-[10vw] text-white flex items-center gap-0">
         <Image alt='' src='/logo.png' height={75} width={65} />
-        <h1 className='font-bold text-lg'>YOOM</h1>
+        <h1 className='font-bold text-lg'>AR Office</h1>
       </div>
       <MobileNav/>
       <div className="user">
